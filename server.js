dbfb82b893ddbb0fb6387f00c73daae796bc4e81
Thanapat Juthavantana <patja60@outlook.co.th>
2018-11-25 04:33:41 +0700

    everything done, except camp type.

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var connection = mysql.createConnection({
 	host: 'localhost',
   port: 3306,
 	user: 'root',
-	password: 'samsen60',
-  database: 'cucamp'
+	password: 'mypassword',
+  database: 'databasename'
 });
 
 app.use(cors())
